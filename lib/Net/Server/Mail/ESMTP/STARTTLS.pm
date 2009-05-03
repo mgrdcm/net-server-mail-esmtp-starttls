37610835f95e3b7d79d3cd42c668235c5d59b52a
Dan Moore <dan@moore.cx>
2009-05-03 00:53:14 -0400

    Freshly created with module-starter.

---
+++ lib/Net/Server/Mail/ESMTP/STARTTLS.pm
@@ -0,0 +1,107 @@
+package Net::Server::Mail::ESMTP::STARTTLS;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Net::Server::Mail::ESMTP::STARTTLS - The great new Net::Server::Mail::ESMTP::STARTTLS!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Net::Server::Mail::ESMTP::STARTTLS;
+
+    my $foo = Net::Server::Mail::ESMTP::STARTTLS->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Dan Moore, C<< <dan at moore.cx> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-net-server-mail-esmtp-starttls at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net-Server-Mail-ESMTP-STARTTLS>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Net::Server::Mail::ESMTP::STARTTLS
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Net-Server-Mail-ESMTP-STARTTLS>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Net-Server-Mail-ESMTP-STARTTLS>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Net-Server-Mail-ESMTP-STARTTLS>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Net-Server-Mail-ESMTP-STARTTLS/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Dan Moore, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Net::Server::Mail::ESMTP::STARTTLS
